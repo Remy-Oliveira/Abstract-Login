5233573135810c836709e25532f34d71f9ab1dac
Remy-Oliveira <remy.musico@gmail.com>
2024-09-26 16:01:39 -0300

    created

---
+++ js.js
@@ -0,0 +1,8 @@
+
+function criarConta() {
+    document.querySelector(".wrapper").classList.add("ativar");
+}
+
+function voltar() {
+    document.querySelector(".wrapper").classList.remove("ativar");
+}
